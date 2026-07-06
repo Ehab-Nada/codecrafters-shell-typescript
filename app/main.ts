ebc4ae25f75ad571be0196df0ad0ebac75836087
Ehab Nada <ehabnada@Ehabs-MacBook-Pro.local>
2026-07-06 16:36:53 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,5 +6,8 @@ const rl = createInterface({
   prompt: "$ ",
 });
 
-// TODO: Uncomment the code below to pass the first stage
 rl.prompt();
+rl.on("line", (line) => {
+  console.error(`${line}: command not found`);
+  rl.prompt();
+});
