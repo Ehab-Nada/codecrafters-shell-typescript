83111c172a9063b00b89fe6d1acf0cc97b03dd14
Ehab Nada <ehabnada@Ehabs-MacBook-Pro.local>
2026-07-06 16:41:28 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -8,6 +8,15 @@ const rl = createInterface({
 
 rl.prompt();
 rl.on("line", (line) => {
+  if(line == "exit") {
+    rl.close();
+    return;
+  }
   console.error(`${line}: command not found`);
   rl.prompt();
 });
+
+// rl.on("close", () => {
+//   console.log("Exiting...");
+//   process.exit(0);
+// });
