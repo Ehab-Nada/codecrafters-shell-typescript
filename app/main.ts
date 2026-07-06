ee56457d0c90a950bce0548a1a1c0fbded1e4ac4
Ehab Nada <ehabnada@Ehabs-MacBook-Pro.local>
2026-07-06 18:07:53 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 import { createInterface } from "readline";
+import { accessSync, constants } from "fs";
+import path from "path";
 
 const rl = createInterface({
   input: process.stdin,
@@ -6,57 +8,65 @@ const rl = createInterface({
   prompt: "$ ",
 });
 
-const builtInCommands = ["echo", "exit","type"];
+const builtInCommands = ["echo", "exit", "type"];
 
 rl.prompt();
 rl.on("line", (line) => {
-
   const parts = line.trim().split(/\s+/);
   const command = parts[0];
   const arg = parts[1];
 
-  if(command == ""){
+  if (command == "") {
     rl.prompt();
     return;
-  }
-  else if(command == "exit") {
+  } else if (command == "exit") {
     rl.close();
     return;
-  }
-  else if (command == "echo") {
+  } else if (command == "echo") {
     console.log(parts.slice(1).join(" "));
     rl.prompt();
     return;
-  } else if (command == "type"){
-
-    if(!arg){
+  } else if (command == "type") {
+    if (!arg) {
       rl.prompt();
       return;
     }
 
     const found = builtInCommands.includes(arg);
-    if(found){
+    if (found) {
       console.log(`${arg} is a shell builtin`);
-
     } else {
-      console.log(`${arg}: not found`);
+
+      const executablePath = findExecutableInPath(arg);
+
+      if(executablePath){
+        console.log(`${arg} is ${executablePath}`);
+
+      } else {
+        console.log(`${arg}: not found`);
+      }
+
     }
     rl.prompt();
-
-  }
-  
-  
-  else {
+  } else {
     console.error(`${command}: command not found`);
     rl.prompt();
   }
-
-  
-
 });
 
 
-// rl.on("close", () => {
-//   console.log("Exiting...");
-//   process.exit(0);
-// });
+
+function findExecutableInPath(command: string): string | null {
+  const pathEnv = process.env.PATH;
+  if (!pathEnv) return null;
+  const directories = pathEnv.split(path.delimiter);
+  for (const dir of directories) {
+    const fullPath = path.join(dir, command);
+    try {
+      accessSync(fullPath, constants.X_OK);
+      return fullPath;
+    } catch {
+    }
+  }
+  return null;
+}
